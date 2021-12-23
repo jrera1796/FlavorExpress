939630b3a5d133e2ec249797242074d58276654f
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-22 19:07:33 -0800

    Added server middleware, rating routes, rating data seed.

--- controllers/api/ratingRoutes.js
+++ controllers/api/ratingRoutes.js
@@ -7,12 +7,13 @@ router.post('/', async (req, res) => {
     const RatingData = await Rating.create({
       rating_comment: req.body.rating_comment,
       rating_score: req.body.rating_score,
-      user_id: req.body.user_id
+      
 
     });
     res.status(200).json(RatingData);
   } catch (err) {
     res.status(400).json(err);
+    res.status(400).json({Status: "Couldn't post"})
   }
 });
 
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const ratingRoutes = require('./ratingRoutes');
-
-router.use('/rating', ratingRoutes);
-
-
-
-
-module.exports = router;
