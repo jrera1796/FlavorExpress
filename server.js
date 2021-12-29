dc69fab0f9d2ff9a71f1ccac21905e5eb7d4bbbd
Kurtis Hight <gears2525@aol.com>
2021-12-28 21:55:45 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening on http://localhost/${PORT}`));
 });
