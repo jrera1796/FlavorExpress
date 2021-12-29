377662d2b74ca50471545d4f422a8ae66bb86a6d
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-28 21:30:52 -0800

    Added rating seeds

--- models/index.js
+++ models/index.js
@@ -1,19 +1,6 @@
 const User = require('./User');
 const Rating = require('./Rating');
 const Recipe = require('./Recipe');
-// const models = {Rating: require('./Rating')}
-
-User.hasMany(Recipe, {
-    foreignKey: 'user_id'
-});
-
-User.hasOne(Rating, {
-    foreignKey: 'user_id'
-});
-
-Rating.belongsTo(Recipe, {
-    foreignKey: 'rating_id'
-})
 
 User.hasMany(Recipe, { foreignKey: 'user_id' });
 
