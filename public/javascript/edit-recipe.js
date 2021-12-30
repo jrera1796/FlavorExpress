d2782bfe632561b2023144a2b9213fdeef044999
kcheykim <kosalcheykim@gmail.com>
2021-12-29 23:24:09 -0800

    creating the edit recipe form

--- public/javascript/edit-recipe.js
+++ public/javascript/edit-recipe.js
@@ -1,21 +1,22 @@
-async function newFormHandler(event) {
+async function editFormHandler(event) {
     event.preventDefault();
 
     const ingredient = document.querySelector('textarea[name="post-ingredients"]').value.trim();
     const direction = document.querySelector('textarea[name="post-direction"]').value.trim();
     const photo_url = document.querySelector('input[name="post-url"]').value;
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
-    const response = await fetch(`/api/recipes`, {
-        method: 'POST',
+    const response = await fetch(`/api/recipes/${id}`, {
+        method: 'PUT',
         body: JSON.stringify({ ingredient, direction, photo_url }),
         headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.replace('/dashboard/');
     } else {
         alert(response.statusText);
     }
 }
 
-document.querySelector('.new-recipe-form').addEventListener('submit', newFormHandler);
+document.querySelector('.edit-recipe-form').addEventListener('submit', editFormHandler);
