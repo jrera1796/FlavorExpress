b78852835dd386f70769380c9144f5e1ad2eae9f
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-01-02 22:48:14 -0800

    Removed local photo route after testing, uploading and getting data from aws

--- controllers/api/photoRoutesLocal.js
+++ controllers/api/photoRoutesLocal.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const multer = require('multer');
-const Recipe = require('../../models/Recipe');
+//===============    THIS SCRIPT IS CURRENTLY NOT IN USE     ===============
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './upload');
@@ -18,22 +18,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 router.post('/single', upload.single('recipe_pic'), (req, res) => {
-  console.log(req.file.path)
-
-  Recipe.update({
-    photo_path: req.file.path
-  }, {
-    // where: { id: 1 }
-  })
-    .then(photoData => {
-      if (!photoData) {
-        res.status(404).json({ message: 'Error' });
-      }
-      res.json(photoData);
-    })
-    .catch(err => { res.status(500).json(err); });
-
-  return console.log("Photo has been uploaded");
+  
+  return console.log("OK-LOCAL");
 });
 
 router.post('/multiple', upload.array('recipe_pic'), (req, res) => {
