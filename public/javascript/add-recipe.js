ef4f60211879fbf7e2e413458d20bb7e32f929d5
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-01-02 18:29:39 -0800

    Recipe photo path

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -3,11 +3,11 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="new-title"]').value;
     const ingredients = document.querySelector('textarea[name="new-ingredients"]').value.trim();
     const direction = document.querySelector('textarea[name="new-direction"]').value.trim();
-    const photo_url = document.querySelector('input[name="new-photo"]').value;
+    const photo_path = document.querySelector('input[name="recipe_pic"]').value;
 
     const response = await fetch(`/api/recipes`, {
         method: 'POST',
-        body: JSON.stringify({ title, ingredients, direction, photo_url }),
+        body: JSON.stringify({ title, ingredients, direction, photo_path }),
         headers: { 'Content-Type': 'application/json' }
     });
 
