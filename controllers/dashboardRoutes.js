21ffb0e207a64efee91a80de907077168ec835ae
Kurtis Hight <gears2525@aol.com>
2022-01-03 21:59:26 -0800

    added recipes handlebars

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,22 +1,41 @@
 const router = require('express').Router();
-const { User, Recipe, Rating } = require('../models');
+const {
+    User,
+    Recipe,
+    Rating
+} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => { //get all recipe for dashboard
     Recipe.findAll({
-        where: { user_id: req.session.user_id },
+        where: {
+            user_id: req.session.user_id
+        },
         attributes: ['id', 'title', 'photo_path', 'created_at'],
         include: [{ //include its associated Rating
                 model: Rating,
                 attributes: ['id', 'rating_comment', 'rating_score'],
-                include: { model: User, attributes: ['username'] }
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
             }, //include its associated User
-            { model: User, attributes: ['username'] }
+            {
+                model: User,
+                attributes: ['username']
+            }
         ]
     }).then(recipeData => {
-        const recipes = recipeData.map(recipe => recipe.get({ plain: true }));
-        res.render('dashboard', { recipes, loggedIn: true });
-    }).catch(err => { res.status(500).json(err); });
+        const recipes = recipeData.map(recipe => recipe.get({
+            plain: true
+        }));
+        res.render('dashboard', {
+            recipes,
+            loggedIn: true
+        });
+    }).catch(err => {
+        res.status(500).json(err);
+    });
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
@@ -25,18 +44,31 @@ router.get('/edit/:id', withAuth, (req, res) => {
         include: [{ //include its associated Rating
                 model: Rating,
                 attributes: ['id', 'rating_comment', 'rating_score'],
-                include: { model: User, attributes: ['username'] }
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
             }, //include its associated User
-            { model: User, attributes: ['username'] }
+            {
+                model: User,
+                attributes: ['username']
+            }
         ]
     }).then(recipeData => {
         if (recipeData) {
-            const recipe = recipeData.get({ plain: true });
-            res.render('edit-recipe', { recipe, loggedIn: true });
+            const recipe = recipeData.get({
+                plain: true
+            });
+            res.render('edit-recipe', {
+                recipe,
+                loggedIn: true
+            });
         } else {
             res.status(404).end();
         }
-    }).catch(err => { res.status(500).json(err); });
+    }).catch(err => {
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
