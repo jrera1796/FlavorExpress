a47ce49838c86080067d3899985f37a098f0044e
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-01-03 18:12:02 -0800

    changes to rating handle bars and form

--- public/javascript/ratingHandler.js
+++ public/javascript/ratingHandler.js
@@ -1,7 +1,19 @@
 async function ratingHandler(event) {
     event.preventDefault();
+<<<<<<< Updated upstream
 
     const rating_comment = document.querySelector('textarea[name="comment-body"]').value.trim();
+=======
+    var c1 = document.getElementById("c1");
+    var chkbox2 = document.getElementById("c2");
+    var chkbox3 = document.getElementById("c3")
+    var chkbox1 = document.getElementById("c4");
+    var chkbox2 = document.getElementById("c5");
+    
+    const rating_comment = document.querySelector('textarea[name="rating-body"]').value.trim();
+    const rating_score = document.querySelector('input[type="checkbox"').value;
+    console.log(rating_score)
+>>>>>>> Stashed changes
     const recipe_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -10,9 +22,17 @@ async function ratingHandler(event) {
         const response = await fetch('/api/ratings', {
             method: 'POST',
             body: JSON.stringify({
+<<<<<<< Updated upstream
                 recipe_id,
                 rating_comment,
                 rating_score
+=======
+                rating_comment,
+                rating_score,
+                user_id,
+                recipe_id
+
+>>>>>>> Stashed changes
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -20,6 +40,10 @@ async function ratingHandler(event) {
         });
 
         if (response.ok) {
+<<<<<<< Updated upstream
+=======
+          debugger;
+>>>>>>> Stashed changes
             document.location.reload();
         } else {
             alert(response.statusText);
@@ -27,4 +51,8 @@ async function ratingHandler(event) {
     }
 }
 
+<<<<<<< Updated upstream
 document.querySelector('.rating-form').addEventListener('submit-rating', ratingHandler);
+=======
+document.querySelector('.rating-form').addEventListener('submit', ratingHandler);
+>>>>>>> Stashed changes
--- utils/helpers.js
+++ utils/helpers.js
@@ -53,5 +53,8 @@ module.exports = {
         //     newStuff += newTemp[i] + thing;
         // }
         // return newStuff;
+    },
+    ifEquals: function(arg1, arg2, options) {
+        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
     }
 }
